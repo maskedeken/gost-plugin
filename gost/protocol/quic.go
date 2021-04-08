f5dc043cdeb3c8986a29212f58766c5b4392a592
Eken Chan <maskedeken@yahoo.com>
2021-04-08 09:00:41 +0800

    fix QUIC disconnecting issue

--- gost/protocol/quic.go
+++ gost/protocol/quic.go
@@ -98,8 +98,7 @@ func NewQUICListener(ctx context.Context) (gost.Listener, error) {
 
 	quicConfig := &quic.Config{
 		ConnectionIDLength:    12,
-		HandshakeIdleTimeout:  time.Second * 8,
-		MaxIdleTimeout:        time.Second * 45,
+		KeepAlive:             true,
 		MaxIncomingStreams:    32,
 		MaxIncomingUniStreams: -1,
 	}
@@ -165,9 +164,8 @@ func (t *QUICTransporter) DialConn() (net.Conn, error) {
 	tlsConfig := buildClientTLSConfig(t.ctx)
 	tlsConfig.NextProtos = []string{"h2", "http/1.1"}
 	quicConfig := &quic.Config{
-		ConnectionIDLength:   12,
-		HandshakeIdleTimeout: time.Second * 8,
-		MaxIdleTimeout:       time.Second * 30,
+		ConnectionIDLength: 12,
+		KeepAlive:          true,
 	}
 	options := t.ctx.Value(C.OPTIONS).(*args.Options)
 	udpAddrStr := options.GetRemoteAddr()
